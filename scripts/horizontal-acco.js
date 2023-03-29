8384bc37dec7c2339041ddfbd3f51c93aad83b23
deprof@rambler.ru <deprof@rambler.ru>
2023-03-29 18:30:04 +0200

    colors section

--- scripts/horizontal-acco.js
+++ scripts/horizontal-acco.js
@@ -1,21 +1,69 @@
-const mesureWidth = () => {
-  return 524;
-}
+(() => {
+  const mesureWidth = (item) => {
+    let reqItemWidth = 0;
 
-const openItem = item => {
-  const hiddenContent = item.find(".colors__content");
-  console.log(hiddenContent);
-  const reqWidth = mesureWidth();
-  console.log(reqWidth);
-  hiddenContent.width(reqWidth);
-}
+    const screenWidth = $(window).width();
+    const container = item.closest(".colors__menu");
+    const titlesBlocks = container.find(".colors__button");
+    const titleWidth = titlesBlocks.width() * titlesBlocks.length;
 
-$(".colors__button").on("click", e => {
-  e.preventDefault();
+    const textContainer = item.find(".colors__container");
+    const paddingLeft = parseInt(textContainer.css("padding-left"));
+    const paddingRight = parseInt(textContainer.css("padding-right"));
+    
+    const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
-  const $this = $(e.currentTarget);
-  console.log($this);
-  const item = $this.closest(".colors__item");
-  console.log(item);
-  openItem(item);
-});
+    if (isMobile) {
+       reqItemWidth = screenWidth - titleWidth;
+    } else {
+      reqItemWidth = 500;
+    }
+
+    return {
+      container: reqItemWidth,
+      textContainer: reqItemWidth - paddingRight - paddingLeft
+    }
+  };
+
+  const closeEveryItemInContainer = (container) => {
+    const items = container.find(".colors__item");
+    const content = container.find(".colors__content");
+
+    items.removeClass("active");  
+    content.width(0);
+  }
+  
+  const openItem = (item) => {
+    const hiddenContent = item.find(".colors__content");
+    const reqWidth = mesureWidth(item);
+    const textBlock = item.find(".colors__container");
+
+    item.addClass("active");
+    hiddenContent.width(reqWidth.container);
+    textBlock.width(reqWidth.textContainer);
+  }
+  
+  $(".colors__button").on("click", (e) => {
+    e.preventDefault();
+  
+    const $this = $(e.currentTarget);  
+    const item = $this.closest(".colors__item");
+    const itemOpened = item.hasClass("active");
+    const container = $this.closest(".colors__menu");
+
+    if (itemOpened) {
+      closeEveryItemInContainer(container)
+    } else {
+      closeEveryItemInContainer(container)
+      openItem(item);
+    }
+  });
+
+  $(".colors__close").on("click", e => {
+    e.preventDefault();
+
+    closeEveryItemInContainer($('.colors__menu'));
+  })
+})()
+
+
